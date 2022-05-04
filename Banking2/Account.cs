8d4c7693fb9f8f4c2bfe88179085cddb4b49cfb3
Sarah Wainscott <swainscott@tql.com>
2022-05-04 19:05:18 -0400

    Add project files.

---
+++ Banking2/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking2 {
+    internal class Account {
+        //properties
+        private static int NextId = 1;
+        public int AcctNbr { get; private set; } = 0;
+        public string AcctName { get; set; } = string.Empty;
+        public decimal Balance  { get; private set; } = 0;
+
+        //methods
+        public bool Deposit(decimal Amount) {
+            if (Amount > 0) {
+                Balance += Amount;
+                Console.WriteLine($"{AcctName}: {Amount:c} added to balance. New balance is {Balance:c}. ");
+                return true;
+                }
+            Console.WriteLine($"Deposit must be greater than 0.");
+            return false;
+            }
+        public bool Withdraw(decimal Amount) {
+            if (Amount < 0) {
+                Console.WriteLine($"Withdraw must be greater than 0");
+                return false;
+                }
+            if (Amount > Balance) {
+                Console.WriteLine($"Insufficient funds in {AcctName}.");
+                return false;
+                }
+            Balance -= Amount;
+            Console.WriteLine($"{AcctName}: {Amount:c} withdrawn. New balance is {Balance:c}.");
+            return true;
+            }
+        public bool Transfer(decimal Amount, Account DAcct) {
+                if (Withdraw(Amount)) {
+                 
+                    DAcct.Deposit(Amount);
+                    Console.WriteLine($"Transfered {Amount:c} from {this.AcctName} to {DAcct.AcctName}. New balance of {DAcct.AcctName} is {DAcct.Balance:c}");
+                    return true;
+                    }
+                Console.WriteLine($"Insufficient funds to transfer from {this.AcctName}.");
+                return false;
+                }
+        public decimal GetBalance() {
+            Console.WriteLine($"{AcctName} Balance: {Balance:c}");
+            return Balance;
+            }
+
+        public Account() {
+            AcctNbr = NextId;
+            NextId++;
+            }
+           
+        }
+    }
---
+++ Banking2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Banking2;
+
+var acct1 = new Account() {
+    AcctName = "Checking"
+    };
+var acct2 = new Account() {
+    AcctName = "Savings"
+    };
+var acct3 = new Account() {
+    AcctName = "House Downpayment"
+    };
+
+acct1.Deposit(1500);
+acct2.Deposit(4000);
+acct3.Deposit(25000);
+
+acct1.GetBalance();
+acct2.GetBalance();
+acct3.GetBalance();
+
+acct1.Withdraw(500);
+acct2.Withdraw(800);
+
+acct2.Transfer(1500, acct3);
+
+Console.WriteLine($"Acct1: {acct1.AcctNbr}. Acct2: {acct2.AcctNbr}. Acct3: {acct3.AcctNbr}");
+
